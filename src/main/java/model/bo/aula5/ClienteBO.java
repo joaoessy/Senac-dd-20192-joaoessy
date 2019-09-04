ae2df3d34be78c0b5ba64427deca6072685e0f18
Aluno <Aluno@S202M04>
2019-09-04 10:42:07 -0300

    aula de hj acabar em casa

--- src/main/java/model/bo/aula5/ClienteBO.java
+++ src/main/java/model/bo/aula5/ClienteBO.java
@@ -3,38 +3,12 @@ package model.bo.aula5;
 import model.dao.aula05.ClienteDAO;
 import model.entity.aula05.Cliente;
 
-
 public class ClienteBO {
-	
-	public void Clientesalvar(Cliente cli) {
-		ClienteDAO cliDAO = new ClienteDAO();
-		
-		
-		
-		
+
+	private ClienteDAO dao = new ClienteDAO();
+
+	public Cliente salvar(Cliente novoCliente) {
+		return dao.salvar(novoCliente);
 	}
-	
-
-	public String salvar(Cliente novoCliente) {
-		String mensagem = "";
-		ClienteDAO clienteDAO = new ClienteDAO();
-
-		if(clienteDAO.CPFcliente(novoCliente.getCpf())) {
-			mensagem = "CPF informado ja pertence a outro cliente";
-		}else {
-			novoCliente = clienteDAO.salvar(novoCliente);
-
-			if(novoCliente.getId() > 0) {
-				mensagem = "cliente cadastrado com sucesso!";
-			}else {
-				mensagem = "Erro ao cadastrar cliente";
-			}
-
-		}
-		return mensagem;
-	}
-
-
 
 }
-
