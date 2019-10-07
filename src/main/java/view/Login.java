57833a4f2660aba72c14077492c67f1816c4f69a
Aluno <Aluno@S202M04>
2019-10-07 10:27:44 -0300

    projeto inicio

---
+++ src/main/java/view/Login.java
@@ -0,0 +1,124 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Toolkit;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textlogin;
+	private JTextField txtSenha;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1091, 729);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 555, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 386, Short.MAX_VALUE)
+		);
+		panel.setLayout(null);
+		
+		Dimension dimensoesTela = Toolkit.getDefaultToolkit().getScreenSize();
+		int larguraDaTela = (int) ((dimensoesTela.getWidth() - 10));
+		int alturaDaTela = (int) (dimensoesTela.getHeight() - 10);
+		
+		JPanel panelLogin = new JPanel();
+		panelLogin.setBounds(720, 345 , 170, 240);
+		panel.add(panelLogin);
+		panelLogin.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("login");
+		lblLogin.setBounds(43, 28, 86, 20);
+		panelLogin.add(lblLogin);
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		txtSenha = new JTextField();
+		txtSenha.setBounds(44, 119, 85, 20);
+		panelLogin.add(txtSenha);
+		txtSenha.setColumns(10);
+		
+		textlogin = new JTextField();
+		textlogin.setBounds(44, 59, 86, 20);
+		panelLogin.add(textlogin);
+		textlogin.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("senha");
+		lblSenha.setBounds(42, 92, 87, 26);
+		panelLogin.add(lblSenha);
+		lblSenha.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBounds(43, 150, 89, 23);
+		panelLogin.add(btnEntrar);
+		
+		JButton btnCadastrarse = new JButton("Cadastrar");
+		btnCadastrarse.setHorizontalAlignment(SwingConstants.LEFT);
+		btnCadastrarse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnCadastrarse.setBounds(43, 184, 89, 23);
+		panelLogin.add(btnCadastrarse);
+		
+		JLabel lblImagemFundo = new JLabel("");
+		lblImagemFundo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblImagemFundo.setIcon(new ImageIcon(Login.class.getResource("/icon_imag/idoso.jpg")));
+		
+		lblImagemFundo.setBounds(0, 0, larguraDaTela, alturaDaTela);
+		panel.add(lblImagemFundo);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
---
+++ src/main/java/view/Primeiratela.java
@@ -0,0 +1,46 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+
+public class Primeiratela {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Primeiratela window = new Primeiratela();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Primeiratela() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+	}
+
+}
